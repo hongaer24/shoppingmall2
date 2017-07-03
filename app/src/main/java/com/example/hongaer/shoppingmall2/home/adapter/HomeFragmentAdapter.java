4938ab2ccd1e5cd2159e0d12df69003dc3fd747a
http://git.oschina.net/madness <736036724@qq.com>
2017-07-03 23:34:11 +0800

    homefragmentadapter 2.0

--- app/src/main/java/com/example/hongaer/shoppingmall2/home/adapter/HomeFragmentAdapter.java
+++ app/src/main/java/com/example/hongaer/shoppingmall2/home/adapter/HomeFragmentAdapter.java
@@ -3,9 +3,21 @@ package com.example.hongaer.shoppingmall2.home.adapter;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.example.hongaer.shoppingmall2.R;
 import com.example.hongaer.shoppingmall2.home.bean.ResultDataBean;
+import com.example.hongaer.shoppingmall2.utils.Constans;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.listener.OnLoadImageListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by hongaer on 2017/7/3.
@@ -13,9 +25,11 @@ import com.example.hongaer.shoppingmall2.home.bean.ResultDataBean;
 
 public class HomeFragmentAdapter extends RecyclerView.Adapter {
     public static final int HOT = 5;
-    private final Context mContext;
     private final ResultDataBean.ResultBean resultbean;
-    private final LayoutInflater mLayoutInflater;
+    private LayoutInflater mLayoutInflater;
+    private Context mContext;
+
+
     /**
      * 五种类型
      */
@@ -46,23 +60,29 @@ public class HomeFragmentAdapter extends RecyclerView.Adapter {
     /**
  *  当前类型
  */
-
     public int currentType = BANNER;
 
     public HomeFragmentAdapter(Context mContext, ResultDataBean.ResultBean resultbean) {
-          this.mContext=mContext;
-          this.resultbean=resultbean;
-          mLayoutInflater=LayoutInflater.from(mContext);
-
+        this.mContext = mContext;
+        this.resultbean = resultbean;
+        this.mLayoutInflater = LayoutInflater.from(mContext);
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        if (viewType == BANNER) {
+            return new BannerViewHolder(mContext,mLayoutInflater.inflate(R.layout.banner_viewpager, null));
+        }
+         return  null;
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+                   if(getItemViewType(position)==BANNER){
+                       BannerViewHolder bannerViewHolder= (BannerViewHolder) holder;
+                       bannerViewHolder.setData(resultbean.getBanner_info());
+                   }
+
 
     }
 
@@ -89,10 +109,39 @@ public class HomeFragmentAdapter extends RecyclerView.Adapter {
                 break;
         }
         return currentType;
+
     }
 
     @Override
     public int getItemCount() {
         return 1;
+    }
+    class BannerViewHolder extends RecyclerView.ViewHolder{
+
+            private Context mContext;
+            private Banner banner;
+        public BannerViewHolder(Context mContext,View itemView ) {
+            super(itemView);
+            this.mContext=mContext;
+            this.banner= (Banner) itemView.findViewById(R.id.banner);
+        }
+
+        public void setData(List<ResultDataBean.ResultBean.BannerInfoBean> banner_info) {
+                       List<String> imagesUrl=new ArrayList<>();
+                        for(int i=0;i<banner_info.size();i++){
+                            String imageUrl=banner_info.get(i).getImage();
+                            imagesUrl.add(imageUrl);
+                        }
+                        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE);
+                        banner.setBannerAnimation(Transformer.Accordion);
+                        banner.setImages(imagesUrl, new OnLoadImageListener() {
+                            @Override
+                            public void OnLoadImage(ImageView view, Object url) {
+
+                                Glide.with(mContext).load(Constans.BASE_URL_IMAGES+url).into(view);
+                            }
+                        });
+
+        }
     }
 }
