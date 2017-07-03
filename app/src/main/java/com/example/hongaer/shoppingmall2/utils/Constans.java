971f15df5e0807d3f5c712fe6e4d1355508e0771
http://git.oschina.net/madness <736036724@qq.com>
2017-07-03 15:59:04 +0800

    utils 1.0

---
+++ app/src/main/java/com/example/hongaer/shoppingmall2/utils/Constans.java
@@ -0,0 +1,10 @@
+package com.example.hongaer.shoppingmall2.utils;
+
+/**
+ * Created by hongaer on 2017/7/3.
+ */
+
+public class Constans {
+
+    public  static  String HOME_URL="http://192.168.3.56:8080/atguigu/json/HOME_URL.json";
+}
