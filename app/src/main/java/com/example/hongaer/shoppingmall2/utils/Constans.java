970ffddfad91b96f36b48cbb6a0601414b486d14
http://git.oschina.net/madness <736036724@qq.com>
2017-07-04 14:48:45 +0800

    constans 1.0

--- app/src/main/java/com/example/hongaer/shoppingmall2/utils/Constans.java
+++ app/src/main/java/com/example/hongaer/shoppingmall2/utils/Constans.java
@@ -6,5 +6,20 @@ package com.example.hongaer.shoppingmall2.utils;
 
 public class Constans {
 
+    public  static  String BASE_URL="http://192.168.3.56:8080/atguigu";
+
+    /**
+     * 主页面路径
+     */
+
+
     public  static  String HOME_URL="http://192.168.3.56:8080/atguigu/json/HOME_URL.json";
+
+
+    /**
+     * 图片路径
+     */
+
+
+    public  static  String BASE_URL_IMAGES=BASE_URL+"/img";
 }
