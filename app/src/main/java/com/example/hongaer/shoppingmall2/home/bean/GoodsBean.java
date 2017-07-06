3bc2f1cd9d6a116ff242a443ce53ec30ab9700b3
http://git.oschina.net/madness <736036724@qq.com>
2017-07-06 14:50:08 +0800

    创建商品类对象，用于存储各个类页面的详情信息

---
+++ app/src/main/java/com/example/hongaer/shoppingmall2/home/bean/GoodsBean.java
@@ -0,0 +1,56 @@
+package com.example.hongaer.shoppingmall2.home.bean;
+
+import java.io.Serializable;
+
+/**
+ * 商品对象
+ */
+
+public class GoodsBean implements Serializable {
+    private String name;
+    private String cover_price;
+    private String figure;
+    private String product_id;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCover_price() {
+        return cover_price;
+    }
+
+    public void setCover_price(String cover_price) {
+        this.cover_price = cover_price;
+    }
+
+    public String getFigure() {
+        return figure;
+    }
+
+    public void setFigure(String figure) {
+        this.figure = figure;
+    }
+
+    public String getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(String product_id) {
+        this.product_id = product_id;
+    }
+
+    @Override
+    public String toString() {
+        return "GoodsBean{" +
+                "name='" + name + '\'' +
+                ", cover_price='" + cover_price + '\'' +
+                ", figure='" + figure + '\'' +
+                ", product_id='" + product_id + '\'' +
+                '}';
+    }
+}
