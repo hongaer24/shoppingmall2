538c4137489b7df709467dcb8a0bb544893e26ef
http://git.oschina.net/madness <736036724@qq.com>
2017-07-09 15:00:30 +0800

    shoppoingCartFragment 2.0 购物车数据页面的适配器设置

--- app/src/main/java/com/example/hongaer/shoppingmall2/shoppingcart/fragmet/ShoppingCartFragment.java
+++ app/src/main/java/com/example/hongaer/shoppingmall2/shoppingcart/fragmet/ShoppingCartFragment.java
@@ -1,13 +1,19 @@
 package com.example.hongaer.shoppingmall2.shoppingcart.fragmet;
 
-import android.graphics.Color;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.example.hongaer.shoppingmall2.R;
 import com.example.hongaer.shoppingmall2.base.BaseFragment;
 import com.example.hongaer.shoppingmall2.home.bean.GoodsBean;
+import com.example.hongaer.shoppingmall2.shoppingcart.adapter.ShoppingCartAdapter;
 import com.example.hongaer.shoppingmall2.shoppingcart.utils.CartStorage;
 
 import java.util.List;
@@ -16,25 +22,79 @@ import java.util.List;
  * Created by hongaer on 2017/7/1.
  */
 
-public class ShoppingCartFragment extends BaseFragment{
+public class ShoppingCartFragment extends BaseFragment implements View.OnClickListener {
+    private TextView tvShopcartEdit;
+    private RecyclerView recyclerview;
+    private LinearLayout llCheckAll;
+    private CheckBox checkboxAll;
+    private TextView tvShopcartTotal;
+    private Button btnCheckOut;
+    private LinearLayout llDelete;
     private TextView textView;
+    private CheckBox cbAll;
+    private Button btnDelete;
+    private Button btnCollection;
+    private ImageView ivEmpty;
+    private TextView tvEmptyCartTobuy;
+    private LinearLayout ll_empty_shopcart;
+    private ShoppingCartAdapter adapter;
+
+    public void onClick(View v) {
+        if ( v == btnCheckOut ) {
+            // Handle clicks for btnCheckOut
+        } else if ( v == btnDelete ) {
+            // Handle clicks for btnDelete
+        } else if ( v == btnCollection ) {
+            // Handle clicks for btnCollection
+        }
+    }
     @Override
     public View initView() {
-        Log.e("tag","主页视图被初始化了");
-        textView = new TextView(mContext);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextSize(25);
-        textView.setTextColor(Color.RED);
-        return textView;
-    }
+        View view = View.inflate(mContext, R.layout.fragment_shopping_cart, null);
+        tvShopcartEdit = (TextView) view.findViewById(R.id.tv_shopcart_edit);
+        recyclerview = (RecyclerView) view.findViewById(R.id.recyclerview);
+        llCheckAll = (LinearLayout) view.findViewById(R.id.ll_check_all);
+        checkboxAll = (CheckBox) view.findViewById(R.id.checkbox_all);
+        tvShopcartTotal = (TextView) view.findViewById(R.id.tv_shopcart_total);
+        btnCheckOut = (Button) view.findViewById(R.id.btn_check_out);
+        llDelete = (LinearLayout) view.findViewById(R.id.ll_delete);
+        cbAll = (CheckBox) view.findViewById(R.id.cb_all);
+        btnDelete = (Button) view.findViewById(R.id.btn_delete);
+        btnCollection = (Button) view.findViewById(R.id.btn_collection);
+        ivEmpty = (ImageView) view.findViewById(R.id.iv_empty);
+        tvEmptyCartTobuy = (TextView) view.findViewById(R.id.tv_empty_cart_tobuy);
+        ll_empty_shopcart= (LinearLayout) view.findViewById(R.id.ll_empty_shopcart);
+
+        btnCheckOut.setOnClickListener(this);
+        btnDelete.setOnClickListener(this);
+        btnCollection.setOnClickListener(this);
+        return view;
+        }
+
+
+
+
     public void initData() {
         super.initData();
         Log.e("tag","主页数据被初始化了");
-        textView.setText("购物车");
+       // textView.setText("购物车");
 
+        showData();
+
+    }
+
+    private void showData() {
         List<GoodsBean> goodsBeanList= CartStorage.getInstance().getAllData();
-              for(int i=0;i<goodsBeanList.size();i++){
-                  Log.i("666687878",goodsBeanList.get(i).toString());
-              }
+          if(goodsBeanList!=null&&goodsBeanList.size()>0){
+               //有数据隐藏
+               ll_empty_shopcart.setVisibility(View.GONE);
+                adapter=new ShoppingCartAdapter(mContext,goodsBeanList);
+                recyclerview.setAdapter(adapter);
+                 recyclerview.setLayoutManager(new LinearLayoutManager(mContext,LinearLayoutManager.VERTICAL,false));
+
+          }else {
+              // 没数据显示
+              ll_empty_shopcart.setVisibility(View.VISIBLE);
+          }
     }
 }
