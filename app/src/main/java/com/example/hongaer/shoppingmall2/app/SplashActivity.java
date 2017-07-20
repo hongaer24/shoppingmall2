05e8a5fe101e36a251c801742171a448c6ca26c8
kobe <736036724@qq.com>
2017-07-20 09:52:55 +0800

    splash 1.0 首次安装判断代码

--- app/src/main/java/com/example/hongaer/shoppingmall2/app/SplashActivity.java
+++ app/src/main/java/com/example/hongaer/shoppingmall2/app/SplashActivity.java
@@ -2,24 +2,38 @@ package com.example.hongaer.shoppingmall2.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 
 import com.example.hongaer.shoppingmall2.R;
 
 public class SplashActivity extends Activity {
+      Handler mHandler=new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
+
+       mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent=new Intent(SplashActivity.this,MainActivity.class);
-                startActivity(intent);
-                finish();
+                SharedPreferences sp=getPreferences(MODE_PRIVATE);
+                boolean isFirst=sp.getBoolean("isFirst",true);
+                Intent intent=new Intent();
+                if(isFirst){
+                    sp.edit().putBoolean("isFirst",false).commit();
+                    //如果是第一次安装进入
+                    intent.setClass(SplashActivity.this,GuideActivity.class);
+
+                }else{
+
+                    intent.setClass(SplashActivity.this,MainActivity.class);
+                }
+                  startActivity(intent);
+                   finish();
 
             }
         },2000);
